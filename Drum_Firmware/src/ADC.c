a12ee011c2f9485eecdef857276673477bbbf527
Beatrix Tarjoto <HuffleDuck@users.noreply.github.com>
2016-01-20 11:34:30 -0800

    Update ADC.c

--- Drum_Firmware/src/ADC.c
+++ Drum_Firmware/src/ADC.c
@@ -14,20 +14,29 @@ void InitADC(void)
 	
 	DIDR0 |= _BV(ADC1D); //disabling digital input for pin A1
 	DIDR0 |= _BV(ADC2D); //disabling digital input for pin A2
+	DIDR0 |= _BV(ADC3D); //disabling digital input for pin A3
+	DIDR0 |= _BV(ADC4D); //disabling digital input for pin A4
 	
 	ADCSRA |= _BV(ADEN);//enable ADC
 	PRR &= ~(_BV(PRADC)); // Disable the power reduction ADC bit
 }
 
-void ChannelOne(void)
+void ChannelZero(void)
 {
 	ADMUX |= _BV(REFS0); // 3.3v refrece voltage
 	ADMUX &= ~ (_BV(REFS1)); // negate REFS1 or else it's left high
 	
 	ADMUX &= 0b11110000; //clear the pin selection
 	ADMUX |= _BV(MUX0); // a1 is input
+	ADMUX |= _BV(MUX1); 
+	ADMUX |= _BV(MUX2); 
+	ADMUX |= _BV(MUX3); 
+	
 	
 	ADCSRA |= _BV(ADSC); // start conversion, use this when ADC is in free running mode to start conversion
 	//otherwise it is triggered by something
 	
+	while(ADSC); // wait for conversion to finish
+	
 }
+
