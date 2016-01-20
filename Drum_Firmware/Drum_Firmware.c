22661cf43b800cfc6989be43b5df068b8b4141dc
Beatrix <dragon.of.joy@gmail.com>
2016-01-19 22:33:13 -0800

    Initial commit, drivers for uart, and timer are included

---
+++ Drum_Firmware/Drum_Firmware.c
@@ -0,0 +1,17 @@
+/*
+ * Drum_Firmware.c
+ *
+ * Created: 1/16/2016 5:02:30 PM
+ *  Author: B-Chan
+ */ 
+
+
+#include <avr/io.h>
+
+int main(void)
+{
+    while(1)
+    {
+        //TODO:: Please write your application code 
+    }
+}
